0c3f88982f2390a6a8da4f160a3561a23a005e8d
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-04 12:32:33 +0200

    la til ovingsoppgave-1-3.py med variabler og utskrifter av navn, alder, høyde og datatype

--- python_ovingsoppgave/ovingsoppgave-1-2.py
+++
@@ -1,9 +0,0 @@
-navn = "Ola"
-alder = 17
-hoyde = 1.75
-liker_python = True
-
-print(navn)
-print(alder)
-print(hoyde)
-print(liker_python)
---
+++ python_ovingsoppgave/ovingsoppgave-1-3.py
@@ -0,0 +1,14 @@
+navn = "Ola"
+alder = 17
+hoyde = 1.75
+liker_python = True
+
+print(navn)
+print(alder)
+print(hoyde)
+print(liker_python)
+
+print(type(navn))
+print(type(alder))
+print(type(hoyde))
+print(type(liker_python))
