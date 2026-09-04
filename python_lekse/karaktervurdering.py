9fcda4ad58468f6e01a4dd8902c8a08925b32ef8
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-04 13:30:21 +0200

    la til karaktervurdering.py for vurdering av poeng

---
+++ python_lekse/karaktervurdering.py
@@ -0,0 +1,14 @@
+poeng = 50
+
+if poeng < 0:
+    print("Ugyldig")
+elif poeng < 50:
+    print("Ikke bestått")
+elif poeng < 70:
+    print("Bestått")
+elif poeng < 90:
+    print("Godt")
+elif poeng <= 100:
+    print("Meget godt")
+else:
+    print("Ugyldig")
