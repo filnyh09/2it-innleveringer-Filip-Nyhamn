9cec97d226be1c6d12aab4f7aabbd1abbc7babd2
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-04 13:17:49 +0200

    la til utprinting

--- python_lekse/personkort.py
+++ python_lekse/personkort.py
@@ -1,4 +1,9 @@
 navn = "Per"
 alder = 16
-Hoyde-i-meter = 1.3
-liker-programmering = True
+Hoyde_i_meter = 1.3
+liker_programmering = True
+
+print("navn: ", navn)
+print("alder: ", alder)
+print("høyde i meter: ", Hoyde_i_meter)
+print("liker programmering: ", liker_programmering)
