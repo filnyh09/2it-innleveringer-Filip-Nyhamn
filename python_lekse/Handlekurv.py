fc7bcfd5a230c14dbe795046ddc6c27866a6a1a8
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-09 21:59:26 +0200

    la til utskriving av baguetpris, energidrikkpris og totalpris.

--- python_lekse/Handlekurv.py
+++ python_lekse/Handlekurv.py
@@ -5,3 +5,8 @@ baguettePris = int(45)
 # antall varer i handlekurven
 energidrikkAntal = int(2)
 baguetteAntal = int(3)
+
+# utreining og utskriving av pris til terminalen
+print(f"totalprisen for energidrikk[ene] blir {energidrikkAntal * energidrikkPris}kr")
+print(f"totalprisen for baguette[ene] blir {baguetteAntal * baguettePris}kr")
+print(f"totalprisen blir {energidrikkAntal * energidrikkPris + baguetteAntal * baguettePris}kr")
