eb8113eea6c2f297753c234a14a4e6c3df247d9d
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-17 13:35:20 +0200

    refactor: update beregn_avstand to return value and added temperature evaluation function

--- LiveKoding/funskjoner.py
+++ LiveKoding/funskjoner.py
@@ -10,6 +10,31 @@ vis_planet("Jord", 1)
 
 def beregn_avstand(fart, timer):
     avstand = fart * timer
-    print(avstand)
+    return avstand
 
-beregn_avstand(1000, 5)
+resultat = beregn_avstand(14128, 13948)
+print(resultat)
+
+def vurder_tempratur(tempratur):
+    if tempratur < -100:
+        return "Extremt kaldt"
+    elif tempratur < 0:
+        return "Kaldt"
+    elif tempratur < 30:
+        return "varmt og godt"
+    else:
+        return "ganske brann"
+
+print(vurder_tempratur(24))
+print(vurder_tempratur(-24))
+print(vurder_tempratur(65))
+print(vurder_tempratur(1))
+print(vurder_tempratur(-6769))
+print(vurder_tempratur(10))
+print(vurder_tempratur(3))
+
+
+def drivstoff_kalkulator_romskip(time, forbrukPrTime):
+    return time * forbrukPrTime
+
+print(drivstoff_kalkulator_romskip(1000, 6769))
