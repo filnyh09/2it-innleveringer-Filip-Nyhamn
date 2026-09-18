9b184d5acacb1bfe7305f5f66a90ac3e1ccab1a0
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-18 21:16:31 +0200

    feat: add satellittmelding function to display satellite names and their locations

---
+++ python_lekse/funksjonsoppgave.py
@@ -0,0 +1,9 @@
+def satellittmelding(navn, planet):
+    print(f"dette er satellitt {navn}")
+    print(f"jeg er lokalisert ved {planet}")
+
+
+
+satellittmelding("MESSENGER", "Merkurius")
+satellittmelding("Venus Express", "Venus")
+satellittmelding("Trace Gas Orbiter", "Mars")
