561d3b37be44e20f8716e0c4456b74daaf9d410d
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-21 09:31:40 +0200

    feat: add signal time estimation function and refactor ping output

--- python_lekse/funksjonsoppgave.py
+++ python_lekse/funksjonsoppgave.py
@@ -5,10 +5,24 @@ def satellittmelding(navn, planet):
 def pingSatellitt(avstand):
     return avstand / 300000
 
+def signalTidEstimate(ping):
+    if ping < 1:
+        print("Direkte kommunikasjon")
+    elif ping < 10:
+        print("Forsinket kommunikasjon")
+    else:
+        print("Stor signalforsinkelse")
+
 
 satellittmelding("MESSENGER", "Merkurius")
 satellittmelding("Venus Express", "Afrodite")
 satellittmelding("Trace Gas Orbiter", "Ares")
 
-print(f"signale bruker {pingSatellitt(560000)} sekunder for å nå målet.")
-print(f"signale bruker {pingSatellitt(32785248125021303121352062106)} sekunder for å nå målet.")
+
+satellittping = pingSatellitt(560000)
+print(f"signale bruker {satellittping} sekunder for å nå målet.")
+signalTidEstimate(satellittping)
+
+satellittping = pingSatellitt(32785248125021303121352062106)
+print(f"signale bruker {satellittping} sekunder for å nå målet.")
+signalTidEstimate(satellittping)
