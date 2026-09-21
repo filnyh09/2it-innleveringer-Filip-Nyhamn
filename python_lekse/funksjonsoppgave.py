3ae85cc996d99427af39c73525a7a50bf508df04
Filip Nyhamn <filnyh09@gvgs.no>
2026-09-21 09:15:25 +0200

    fix: correct satellite names and improve ping calculation function

--- python_lekse/funksjonsoppgave.py
+++ python_lekse/funksjonsoppgave.py
@@ -2,8 +2,13 @@ def satellittmelding(navn, planet):
     print(f"dette er satellitt {navn}")
     print(f"jeg er lokalisert ved {planet}")
 
+def pingSatellitt(avstand):
+    return avstand / 300000
 
 
 satellittmelding("MESSENGER", "Merkurius")
-satellittmelding("Venus Express", "Venus")
-satellittmelding("Trace Gas Orbiter", "Mars")
+satellittmelding("Venus Express", "Afrodite")
+satellittmelding("Trace Gas Orbiter", "Ares")
+
+print(f"signale bruker {pingSatellitt(560000)} sekunder for å nå målet.")
+print(f"signale bruker {pingSatellitt(32785248125021303121352062106)} sekunder for å nå målet.")
